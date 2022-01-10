73e1c444f5d13a7892266492d6cd436f7fbd1602
vishant007 <vishant9797@gmail.com>
2022-01-10 21:10:10 +0530

    program_to_find_subsets_of_set

---
+++ subsets.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void subsets(int arr[], int n){
+    for(int i =0; 1<<n; i++){
+        for(int j =0; j<n; j++){
+            cout<<arr[j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int arr[4] = {1,2,3,4};
+    subsets(arr,4);
+
+    return 0;
+}
