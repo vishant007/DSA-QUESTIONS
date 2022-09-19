e4e8d1e3151ea1fcdeca43cbbc7dd9058fc521a3
Vishant Shah <vishant9797@gmail.com>
2022-09-19 19:04:28 +0530

    imp question for on-campus placements

---
+++ queues/circulartour.cpp
@@ -0,0 +1,38 @@
+//https://practice.geeksforgeeks.org/problems/circular-tour
+
+
+struct petrolPump{
+    int petrol;
+    int distance;
+};
+
+
+
+class Solution{
+  public:
+  
+    
+    int tour(petrolPump p[],int n)
+    {
+        int deficit = 0;
+        int balance = 0;
+        int start = 0;
+        
+        for(int i=0;i<n;i++){
+            balance += p[i].petrol - p[i].distance;
+            if(balance<0){
+                deficit += balance; 
+                start=i+1;
+                balance = 0;
+            }
+        }
+       //Your code here
+       if(deficit+balance>=0){
+           return start;
+       }
+       else{
+           return -1;
+       }
+    }
+};
+
